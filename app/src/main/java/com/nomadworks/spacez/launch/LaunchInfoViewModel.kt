82d11fe69ea318944e552af7b7ec1de9f3a023b3
dukhocho <dukhochoi@gmail.com>
2021-01-03 15:08:21 +1100

    Format latest launch info

--- app/src/main/java/com/nomadworks/spacez/launch/LaunchInfoViewModel.kt
+++ app/src/main/java/com/nomadworks/spacez/launch/LaunchInfoViewModel.kt
@@ -4,17 +4,28 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nomadworks.spacez.api.model.launch.Launch
 import com.nomadworks.spacez.repository.SpacexRepository
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
-class LaunchInfoViewModel(private val repository: SpacexRepository) : ViewModel(), LaunchInfoContract.ViewModel {
+class LaunchInfoViewModel(private val repository: SpacexRepository) : ViewModel(),
+    LaunchInfoContract.ViewModel {
     data class ViewState(
         val launchInfo: String,
+        val launchName: String,
+        val patchImageLink: String?,
+        val launchImageLink: String?,
         val showWait: Boolean
     )
 
-    private var viewStateSnapshot = ViewState(launchInfo = "", showWait = false)
+    private var viewStateSnapshot = ViewState(
+        showWait = false,
+        launchInfo = "",
+        launchName = "",
+        patchImageLink = null,
+        launchImageLink = null
+    )
     private val mutableViewState = MutableLiveData<ViewState>(
         viewStateSnapshot
     )
@@ -27,10 +38,39 @@ class LaunchInfoViewModel(private val repository: SpacexRepository) : ViewModel(
 
     override fun requestLaunchInfo() {
         viewModelScope.launch {
-            updateViewState(viewStateSnapshot.copy(launchInfo = "Fetching...", showWait = true))
+            updateViewState(
+                viewStateSnapshot.copy(
+                    launchInfo = "Fetching...",
+                    launchName = "",
+                    launchImageLink = null,
+                    patchImageLink = null,
+                    showWait = true
+                )
+            )
             val launch = repository.fetchLatestLaunch()
             Timber.d("[space] launch result from viewmodel: $launch")
-            updateViewState(viewStateSnapshot.copy(launchInfo = "$launch", showWait = false))
+            updateLaunchInfo(launch)
         }
+    }
+
+    private fun updateLaunchInfo(launch: Launch) {
+        updateViewState(viewStateSnapshot.copy(
+            launchInfo = "${launch.details}",
+            launchName = "${launch.name}",
+            launchImageLink = launch.links?.let {
+                if (it.flickr?.original?.isNotEmpty() == true) {
+                    it.flickr.original[0]
+                } else {
+                    null
+                }
+            },
+            patchImageLink = launch.links?.let {
+                if (it.patch?.small?.isNotBlank() == true) {
+                    it.patch.small
+                } else {
+                    null
+                }
+            },
+            showWait = false))
     }
 }
