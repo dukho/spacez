1885999162350ed7b26107d6b48964a739bfaeac
Dukho Choi <dukhochoi@gmail.com>
2021-02-06 21:06:01 +1100

    Add FB settings (#3)
    * Add FB settings
    
    * Add firebase analytics instance to the base activity
    
    Co-authored-by: woolies-dchoi <dchoi@woolworths.com.au>

--- app/src/main/java/com/nomadworks/spacez/MainActivity.kt
+++ app/src/main/java/com/nomadworks/spacez/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.nomadworks.spacez
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.nomadworks.spacez.common.base.BaseActivity
 import com.nomadworks.spacez.databinding.ActivityMainBinding
 import com.nomadworks.spacez.launch.LaunchInfoActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         activityMainBinding.btnLaunch.setOnClickListener {
             startActivity(
                 Intent(this, LaunchInfoActivity::class.java)
             )
+        }
+
+        trackAppLaunch()
+    }
+
+    private fun trackAppLaunch() {
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            Bundle().putString("screen_name", "main")
         }
     }
 }
